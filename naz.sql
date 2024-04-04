fb558e18c30635de0f2d4be930f7969a32856c98
nazeemaqa <149041752+nazeemaqa@users.noreply.github.com>
2024-04-04 18:04:13 +0530

    Add files via upload

---
+++ naz.sql
@@ -0,0 +1,11 @@
+create database school;
+use school;
+create table student(Id int,Name varchar(20),Gender varchar(20));
+select * from student;
+/*
+create  database world;
+use world;
+select Name, population from country where continent='Asia';
+-- select *from country where continent<>'Asia';
+*/
+
